9d773a6dacc9b88edd795045909d5e7636320fff
bleclerc <brice.leclerc@wanadoo.fr>
2025-12-06 19:57:04 +0100

    chore: angular 20

--- front/src/app/ui/header/header.component.ts
+++ front/src/app/ui/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
     selector: 'reborn-header',
     imports: [TranslateModule],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+    styleUrl: './header.component.scss',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush
+    //todo: rule eslint detection onpush
+    //todo: rule eslint standalone true
 })
 export class HeaderComponent {
 
